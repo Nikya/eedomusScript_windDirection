7ea93503031aa93d53aa156b0c63c95b49327743
Yannick Severac <severac.yannick@gmail.com>
2015-11-20 16:38:07 +0100

    Version fonctionel

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
 # eedomus script : wind direction
-Un script pour la box domotique eedomus pour convertir la direction du vent 
+
+Version : 1.0
+
+Auteur : Nikya
 
 
+## Description
+*eedomusScript_windDirection* est un script pour la box domotique *eedomus*.
+
+Il permet de convertir une direction de vent en degré vers son équivalent en point cardinal.
+
+Testé avec L'anémomètre / girouette *Oregon Scientific WGR800* ![WGR800](https://secure.eedomus.com/img/mdm/periph/WGR800.png "Oregon Scientific WGR800")
+
+## Installation
+1. Télécharger le projet sur GitHub : [github.com/Nikya/eedomusScript_windDirection](https://github.com/Nikya/eedomusScript_windDirection/archive/master.zip)
+1. Uploader le fichier *wind_direction.php* sur la box ([Doc eedomus scripts](http://doc.eedomus.com/view/Scripts#Script_HTTP_sur_la_box_eedomus))
+
+## Tester
+Ce script peut ensuite être testé au travers du lien suivant dans le naviguateur
+
+	http://[ip_de_votre_box]/script/?exec=wind_direction.php&periph=[code_api_perif]
+
+Où il faut remplacer
+- *[ip_de_votre_box]* : par l'IP de votre Box *eedomus*
+- *[code_api_perif]* : par le code API du capteur qui renvoie la direction du vent en °
+
+Exemple
+
+	http://192.168.1.2/script/?exec=wind_direction.php&periph=999999
+
+## Résultat
+Le résultat est au format XML.
+
+Exemple de résulat
+```xml
+<data>
+	<change>2015-Nov-20 14:07:43</change>
+	<degree>256</degree>
+	<div>22</div>
+	<fr>
+		<short>OSO</short>
+		<long>Ouest-Sud-Ouest</long>
+	</fr>
+	<en>
+		<short>WSW</short>
+		<long>West-South-West</long>
+	</en>
+</data>
+```
+
+## Correspondance XPATH
+
+Les diférentes informations possibles retournées par les Xpath suivants :
+
+- ```/data/change``` : La date et l'heure de la dernière actualisation du capteur de direction du vent
+- ```/data/degree``` : Orientation du vent mesuré en degré (°)
+- ```/data/div``` : Division correspondante à l'orientation (360° divisé en  32 divisions numérotées de 0 à 31 dans le sens horaire)
+- ```/data/fr/short``` : Orientation du vent exprimé sous forme de *point cardinal* **court** en **français**. Exemple NO
+- ```/data/fr/long``` : Orientation du vent exprimé sous forme de *point cardinal* **long** en **français**. Exemple Nord-Ouest
+- ```/data/en/short``` : Orientation du vent exprimé sous forme de *point cardinal* **court** en **Anglais**. Exemple NW
+- ```/data/en/long``` : Orientation du vent exprimé sous forme de *point cardinal* **long** en **Anglais**. Exemple North-West
+
+## Un exemple d'exploitation avec l'eedomus
+
+Créer un nouveau périphérique de type *HTTP Capteur*
+
+Renseigner les paramètres suivants :
+
+- Type de données : ```Texte```
+- URL de la requête : ```http://[VAR1]/script/?exec=wind_direction.php&periph=[VAR2]```
+- Chemin XPATH : ```/data/fr/long```
+- Fréquence de la requête : ```5```
+- Variables utilisateur :
+	- [VAR1] : ```Localhost```
+	- [VAR2] : ```999999``` Le code API du périphérique
---
+++ wind_direction.php
@@ -0,0 +1,220 @@
+<?php
+
+/*******************************************************************************
+* Un script pour la box domotique eedomus pour convertir la direction du vent
+********************************************************************************
+* Version :
+*	1.0
+*
+* Auteur :
+*	Nikya
+*	https://github.com/Nikya/
+*
+* Documentation complète :
+* 	https://github.com/Nikya/eedomusScript_windDirection
+*
+* Param :
+*	periph : L'identifiant (Code API) du periferique mesurant le vent
+*
+* Retour :
+* 	XML : Résultat formaté au format XML
+*
+*******************************************************************************/
+
+// Seulement utile en mode test
+//require_once ("../eedomusScriptsEmulator.php");
+
+////////////////////////////////////////////////////////////////////////////////
+// Definition des points cardianux
+$cardinalArray = array(
+	'fr' => array(
+		'short' => array(
+			array('N', '', ''),
+			array('N', 'N', 'E'),
+			array('N', 'N', 'E'),
+			array('N', 'E', ''),
+			array('N', 'E', ''),
+			array('E', 'N', 'E'),
+			array('E', 'N', 'E'),
+			array('E', '', ''),
+			array('E', '', ''),
+			array('E', 'S', 'E'),
+			array('E', 'S', 'E'),
+			array('S', 'E', ''),
+			array('S', 'E', ''),
+			array('S', 'S', 'E'),
+			array('S', 'S', 'E'),
+			array('S', '', ''),
+			array('S', '', ''),
+			array('S', 'S', 'O'),
+			array('S', 'S', 'O'),
+			array('S', 'O', ''),
+			array('S', 'O', ''),
+			array('O', 'S', 'O'),
+			array('O', 'S', 'O'),
+			array('O', '', ''),
+			array('O', '', ''),
+			array('O', 'N', 'O'),
+			array('O', 'N', 'O'),
+			array('N', 'O', ''),
+			array('N', 'O', ''),
+			array('N', 'N', 'O'),
+			array('N', 'N', 'O'),
+			array('N', '', ''),
+		),
+		'long' => array(
+			array('Nord', '', ''),
+			array('Nord', '-Nord', '-Est'),
+			array('Nord', '-Nord', '-Est'),
+			array('Nord', '-Est', ''),
+			array('Nord', '-Est', ''),
+			array('Est', '-Nord', '-Est'),
+			array('Est', '-Nord', '-Est'),
+			array('Est', '', ''),
+			array('Est', '', ''),
+			array('Est', '-Sud', '-Est'),
+			array('Est', '-Sud', '-Est'),
+			array('Sud', '-Est', ''),
+			array('Sud', '-Est', ''),
+			array('Sud', '-Sud', '-Est'),
+			array('Sud', '-Sud', '-Est'),
+			array('Sud', '', ''),
+			array('Sud', '', ''),
+			array('Sud', '-Sud', '-Ouest'),
+			array('Sud', '-Sud', '-Ouest'),
+			array('Sud', '-Ouest', ''),
+			array('Sud', '-Ouest', ''),
+			array('Ouest', '-Sud', '-Ouest'),
+			array('Ouest', '-Sud', '-Ouest'),
+			array('Ouest', '', ''),
+			array('Ouest', '', ''),
+			array('Ouest', '-Nord', '-Ouest'),
+			array('Ouest', '-Nord', '-Ouest'),
+			array('Nord', '-Ouest', ''),
+			array('Nord', '-Ouest', ''),
+			array('Nord', '-Nord', '-Ouest'),
+			array('Nord', '-Nord', '-Ouest'),
+			array('Nord', '', ''),
+		)
+	),
+	'en' => array(
+		'short' => array(
+			array('N', '', ''),
+			array('N', 'N', 'E'),
+			array('N', 'N', 'E'),
+			array('N', 'E', ''),
+			array('N', 'E', ''),
+			array('E', 'N', 'E'),
+			array('E', 'N', 'E'),
+			array('E', '', ''),
+			array('E', '', ''),
+			array('E', 'S', 'E'),
+			array('E', 'S', 'E'),
+			array('S', 'E', ''),
+			array('S', 'E', ''),
+			array('S', 'S', 'E'),
+			array('S', 'S', 'E'),
+			array('S', '', ''),
+			array('S', '', ''),
+			array('S', 'S', 'W'),
+			array('S', 'S', 'W'),
+			array('S', 'W', ''),
+			array('S', 'W', ''),
+			array('W', 'S', 'W'),
+			array('W', 'S', 'W'),
+			array('W', '', ''),
+			array('W', '', ''),
+			array('W', 'N', 'W'),
+			array('W', 'N', 'W'),
+			array('N', 'W', ''),
+			array('N', 'W', ''),
+			array('N', 'N', 'W'),
+			array('N', 'N', 'W'),
+			array('N', '', ''),
+		),
+		'long' => array(
+			array('North', '', ''),
+			array('North', '-North', '-East'),
+			array('North', '-North', '-East'),
+			array('North', '-East', ''),
+			array('North', '-East', ''),
+			array('East', '-North', '-East'),
+			array('East', '-North', '-East'),
+			array('East', '', ''),
+			array('East', '', ''),
+			array('East', '-South', '-East'),
+			array('East', '-South', '-East'),
+			array('South', '-East', ''),
+			array('South', '-East', ''),
+			array('South', '-South', '-East'),
+			array('South', '-South', '-East'),
+			array('South', '', ''),
+			array('South', '', ''),
+			array('South', '-South', '-West'),
+			array('South', '-South', '-West'),
+			array('South', '-West', ''),
+			array('South', '-West', ''),
+			array('West', '-South', '-West'),
+			array('West', '-South', '-West'),
+			array('West', '', ''),
+			array('West', '', ''),
+			array('West', '-North', '-West'),
+			array('West', '-North', '-West'),
+			array('North', '-West', ''),
+			array('North', '-West', ''),
+			array('North', '-North', '-West'),
+			array('North', '-North', '-West'),
+			array('North', '', ''),
+		)
+	)
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Lecture du paramêtre du script
+$periphId = getArg('periph', true);
+
+////////////////////////////////////////////////////////////////////////////////
+// Lecture de la valeur
+$aVal = getValue($periphId);
+$value = $aVal['value'];
+$change = $aVal['change'];
+
+////////////////////////////////////////////////////////////////////////////////
+// Recherche de la division
+$step = 360/32;
+$divId = 0;
+
+for ($d=0; $d<=360; $d+=$step) {
+	if ($value>=$d and $value<=$d+$step)
+		break;
+	$divId++;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Conversion de la division en point cardinaux
+$cardinal_fr_s = $cardinalArray['fr']['short'][$divId][0].$cardinalArray['fr']['short'][$divId][1].$cardinalArray['fr']['short'][$divId][2];
+$cardinal_fr_l = $cardinalArray['fr']['long'][$divId][0].$cardinalArray['fr']['long'][$divId][1].$cardinalArray['fr']['long'][$divId][2];
+$cardinal_en_s = $cardinalArray['en']['short'][$divId][0].$cardinalArray['en']['short'][$divId][1].$cardinalArray['en']['short'][$divId][2];
+$cardinal_en_l = $cardinalArray['en']['long'][$divId][0].$cardinalArray['en']['long'][$divId][1].$cardinalArray['en']['long'][$divId][2];
+
+////////////////////////////////////////////////////////////////////////////////
+// Renvoie du resultat en XML
+$content_type = 'text/xml';
+sdk_header($content_type);
+
+echo <<<EOD
+	<data>
+		<change>$change</change>
+		<degree>$value</degree>
+		<div>$divId</div>
+		<fr>
+			<short>$cardinal_fr_s</short>
+			<long>$cardinal_fr_l</long>
+		</fr>
+		<en>
+			<short>$cardinal_en_s</short>
+			<long>$cardinal_en_l</long>
+		</en>
+	</data>
+EOD;
+?>
